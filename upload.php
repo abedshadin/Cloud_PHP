47b027320fd63f2dee6b711ac7b6192f5f27ab35
abedshadin <abedshadin@gmail.com>
2023-07-15 14:32:28 +0600

    up updte

--- upload.php
+++ upload.php
@@ -67,26 +67,24 @@ if (isset($_GET['logout'])) {
 
 
 
-
-
-<?php
-$search_value=$_SESSION["username"];
+    <?php
+$search_value = $_SESSION["username"];
 $target_dir = "uploads/$search_value/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-// // Check if image file is a actual image or fake image
-// if(isset($_POST["submit"])) {
-//   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-//   if($check !== false) {
-//     echo "File is an image - " . $check["mime"] . ".";
-//     $uploadOk = 1;
-//   } else {
-//     echo "File is not an image.";
-//     $uploadOk = 0;
-//   }
-// }
+$filename = basename($_FILES["fileToUpload"]["name"]);
+$randomString = uniqid();
+
+// Get the extension of the uploaded file
+$extension = pathinfo($filename, PATHINFO_EXTENSION);
+
+// Remove the extension from the filename
+$filenameWithoutExtension = pathinfo($filename, PATHINFO_FILENAME);
+
+// Concatenate the random value after the file name
+$target_file = $target_dir . $filenameWithoutExtension . "_" . $randomString . "." . $extension;
+
+$uploadOk = 1;
+$imageFileType = strtolower($extension);
 
 // Check if file already exists
 if (file_exists($target_file)) {
@@ -94,33 +92,18 @@ if (file_exists($target_file)) {
   $uploadOk = 0;
 }
 
-// // Check file size
-// if ($_FILES["fileToUpload"]["size"] > 500000) {
-//   echo "Sorry, your file is too large.";
-//   $uploadOk = 0;
-// }
-
-// // Allow certain file formats
-// if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-// && $imageFileType != "gif" ) {
-//   echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-//   $uploadOk = 0;
-// }
-
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-  echo " Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
+  echo "Sorry, your file was not uploaded.";
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo  header("Location: index.php");
-    
-   
+    echo header("Location: index.php");
   } else {
     echo "Sorry, there was an error uploading your file.";
   }
 }
 ?>
+
 <center><br><a href="index.php">Back To Home Page</a></center>
 
 </body>
